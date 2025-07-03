fca519e21c98d5b1755a564a9f9fc6f76ecf7cdd
HamzaKotbAddin <hamza.yasin.dev@gmail.com>
2025-07-03 14:40:03 +0300

    server auth models routes controll

---
+++ server/controllers/auth.controller.ts
@@ -0,0 +1,60 @@
+import { NextFunction , Request, Response } from "express";
+import User from "../models/user.model.ts";
+import { sign } from "jsonwebtoken";
+
+
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+const maxAgeSeconds = maxAge / 1000;
+const createToken = (email: string , userId: string) => {
+    try{
+        if (!process.env.JWT_KEY) {
+            throw new Error("JWT_KEY is not defined in environment variables");
+        }
+        return sign({ email, userId }, process.env.JWT_KEY, {
+            expiresIn: maxAgeSeconds
+        });
+    } catch (error) {
+        console.error("Error creating token:", error);
+        throw error;
+    }
+};
+
+export const signUp = async (req: Request, res: Response, next: NextFunction): Promise<any> =>  {
+  try {
+    const { username, email, password } = req.body;
+
+    if ( !email || !password) {
+      return res.status(400).json({ message: `username, email and password are required` });
+    }
+
+    const user = await User.create({
+      username,
+      email,
+      password
+    })
+    if (!user) {
+      return res.status(400).json({ message: "User creation failed" });
+    }
+
+    const token = createToken(email, user.id);
+    res.cookie("jwt", token, {
+      httpOnly: true,
+      maxAge: maxAge,
+      secure: true,
+      sameSite: "strict", // Prevent CSRF attacks
+    });
+    res.status(201).json({ message: "User created successfully", user: {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+
+      image: user.image,
+      profileSetup: user.profileSetup
+    } });
+
+  } catch (error) {
+    console.error("Error during sign up:", error);
+    res.status(500).json({ message: "Internal server error" });
+    next(error);
+  }
+}
---
+++ server/models/user.model.ts
@@ -0,0 +1,67 @@
+import mongoose from "mongoose";
+import { genSalt, hash} from "bcrypt";
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    validate: {
+      validator: (v: string) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(v);
+      },
+      message: (props: { value: string }) => `${props.value} is not a valid email!`,
+    },
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  firstName: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: false,
+    default: "https://www.gravatar.com/avatar/placeholder",
+  },
+  color: {
+    type: Number,
+    required: false,
+    default: 0x000000,
+  },
+  profileSetup: {
+    type: Boolean,
+    default: false,
+  }
+
+});
+
+userSchema.pre("save", async function hashPassword(next) {
+try {
+    const salt = await genSalt(10);
+    this.password = await hash(this.password, salt);
+    next();
+} catch (error) {
+    console.error(" ❌ Error hashing password:", error);
+    next(error);
+}
+});
+
+ const User = mongoose.model("User", userSchema);
+
+export default User;
